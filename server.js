014e3eaaa8e4d65605b95dbcf549a4f268ee00f1
Brian Kelly <brnklly@gmail.com>
2019-01-15 14:20:53 -0600

    Added User, Group and Choice routes

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 
+// import routes
+const users = require("./routes/api/users");
+const groups = require("./routes/api/groups");
+const choices = require("./routes/api/choices");
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to PickOne");
 });
+
+// use routes
+app.use("/api/users", users);
+app.use("/api/groups", groups);
+app.use("/api/choices", choices);
 
 const port = process.env.PORT || 5000;
 
